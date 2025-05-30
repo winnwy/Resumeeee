bbe8b387ed75298553e68922bc7957a5916ea81b
Winn Z <winnwyz@gmail.com>
2025-05-30 17:10:00 +1000

    chore:format

--- src/components/RawLink.tsx
+++ src/components/RawLink.tsx
@@ -8,16 +8,21 @@ type RawLinkProps = {
   className?: string;
 };
 
-const RawLink = ({ href, children, currentStyle, className="" }: RawLinkProps) => {
+const RawLink = ({
+  href,
+  children,
+  currentStyle,
+  className = "",
+}: RawLinkProps) => {
   return (
-      <a
-        href={href}
-        className={`${currentStyle.accent} ${currentStyle.hover} ${className} font-mono font-bold transition-colors duration-200`}
-        style={{ textShadow: `0 0 10px currentColor` }}
-        target="none"
-      >
-        [{children}]
-      </a>
+    <a
+      href={href}
+      className={`${currentStyle.accent} ${currentStyle.hover} ${className} font-mono font-bold transition-colors duration-200`}
+      style={{ textShadow: `0 0 10px currentColor` }}
+      target="none"
+    >
+      [{children}]
+    </a>
   );
 };
 
